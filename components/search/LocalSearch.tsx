12da8d1fbb54ffadeba680b73eede81be4d8ae29
Vinod123456183 <vinodsinghbarti420@gmail.com>
2026-08-30 14:24:36 +0530

    Fixing-Backend-Infinite-Refreshing

--- components/search/LocalSearch.tsx
+++ components/search/LocalSearch.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-// important useRouter will come from here , nothing else
-
 import Image from 'next/image'
 import { useSearchParams, useRouter, usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
@@ -11,70 +9,67 @@ import { formUrlQuery, removeKeysFromUrlQuery } from '@/lib/url'
 import { Input } from '../ui/input'
 
 interface Props {
-  route: 'string'
-  imgSrc: 'string'
-  placeholder: 'string'
-  otherClasses?: 'string'
+  route: string
+  imgSrc: string
+  placeholder: string
+  otherClasses?: string
 }
 
 function LocalSearch({ route, imgSrc, placeholder, otherClasses }: Props) {
   const router = useRouter()
   const pathname = usePathname()
-
   const searchParams = useSearchParams()
+
   const query = searchParams.get('query') || ''
 
   const [searchQuery, setSearchQuery] = useState(query)
 
   useEffect(() => {
-    // imp
     const delayDebounceFn = setTimeout(() => {
+      // Don't push if URL already has the same query
+      if (searchQuery === query) return
+
       if (searchQuery) {
         const newUrl = formUrlQuery({
           params: searchParams.toString(),
           key: 'query',
           value: searchQuery,
         })
-        router.push(newUrl, { scroll: false })
-      } else {
-        if (pathname === route) {
-          const newUrl = removeKeysFromUrlQuery({
-            params: searchParams.toString(),
-            keysToRemove: ['query'],
-          })
 
-          router.push(newUrl, { scroll: false })
-        }
+        router.push(newUrl, { scroll: false })
+      } else if (pathname === route) {
+        const newUrl = removeKeysFromUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ['query'],
+        })
+
+        router.push(newUrl, { scroll: false })
       }
     }, 300)
 
     return () => clearTimeout(delayDebounceFn)
-  }, [searchQuery, route, route, searchParams, pathname])
+  }, [searchQuery, query, route, searchParams, pathname, router])
 
   return (
-    <>
-      <div
-        className={`background-light800_darkgradient item-center rounded-[10px]px-4 flex min-h-[56px] grow gap-4 ${otherClasses}`}
-      >
-        {searchParams.toString()}
-        <Image
-          src={imgSrc}
-          width={24}
-          height={24}
-          alt="Search"
-          className="cursor-pointer"
-        />
-        <Input
-          type="text"
-          placeholder={placeholder}
-          value={searchQuery}
-          onChange={(e) => {
-            setSearchQuery(e.target.value)
-          }}
-          className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
-        />
-      </div>
-    </>
+    <div
+      className={`background-light800_darkgradient item-center flex min-h-[56px] grow gap-4 rounded-[10px] px-4 ${otherClasses}`}
+    >
+      <Image
+        src={imgSrc}
+        width={24}
+        height={24}
+        alt="Search"
+        className="cursor-pointer"
+      />
+
+      <Input
+        type="text"
+        placeholder={placeholder}
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="paragraph-regular no-focus placeholder text-dark400_light700 border-none shadow-none outline-none"
+      />
+    </div>
   )
 }
 
